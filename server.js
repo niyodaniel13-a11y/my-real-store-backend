a42205d1a27cabc1cef60e9f3c57d2797a3f021f
niyodaniel13-a11y <niyodaniel13@gmail.com>
2025-12-27 08:21:50 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+body{font-family:Arial;}
+console.log('Frontend ready');
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Icyanzu-style Store</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <h1>Welcome / Murakaza neza</h1>
+  <div id="products"></div>
+  <script src="js/script.js"></script>
+</body>
+</html>
